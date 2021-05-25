57a7a75f5758474dd23d99d3a4e2c7451f92839c
alfaelearning <61777263+alfaelearning@users.noreply.github.com>
2021-05-26 04:34:47 +0530

    spring boot jetty

---
+++ src/main/java/com/infybuzz/app/SpringBootJettyApplication.java
@@ -0,0 +1,13 @@
+package com.infybuzz.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootJettyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootJettyApplication.class, args);
+	}
+
+}
